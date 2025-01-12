6863e3cf7bb5c912c57a9d60c37f3b8c76de5144
nhubaole <okamisameko7123@gmail.com>
2025-01-12 15:39:49 +0700

    update configs

--- internal/dataaccess/message.sql.go
+++ internal/dataaccess/message.sql.go
@@ -12,9 +12,16 @@ import (
 )
 
 const getMessageByConversationID = `-- name: GetMessageByConversationID :many
-SELECT id, conversation_id, sender_id, type, content, rent_auto_content::text,  created_at
+SELECT id, 
+       conversation_id, 
+       sender_id, 
+       type, 
+       content, 
+       rent_auto_content::text, 
+       created_at
 FROM public.messages
 WHERE conversation_id = $1
+ORDER BY created_at DESC
 `
 
 type GetMessageByConversationIDRow struct {
--- sql/queries/message.sql
+++ sql/queries/message.sql
@@ -8,6 +8,14 @@ FROM public.messages
 WHERE id = $1;
 
 -- name: GetMessageByConversationID :many
-SELECT id, conversation_id, sender_id, type, content, rent_auto_content::text,  created_at
+SELECT id, 
+       conversation_id, 
+       sender_id, 
+       type, 
+       content, 
+       rent_auto_content::text, 
+       created_at
 FROM public.messages
-WHERE conversation_id = $1;
+WHERE conversation_id = $1
+ORDER BY created_at DESC;
+
